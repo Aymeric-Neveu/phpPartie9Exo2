186328e6f8c68c9f5ff521e89593b49652b8431d
root <emploi.neveu@gmail.com>
2018-12-03 11:54:14 +0100

    exo2

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 2</title>
+  </head>
+  <body>
+    <?php
+    $today = date('j-m- y');
+    echo 'Nous sommes le' . ' ' .$today;
+    ?>
+  </body>
+</html>
